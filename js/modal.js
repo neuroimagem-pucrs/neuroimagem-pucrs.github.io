133ae98a5575caf187508b01f3433f6043121156
José Osmar <ze_osmar@hotmail.com>
2017-07-26 12:36:41 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -6,3 +6,16 @@ $(document).ready(function() {
         $('#popup .modal-body').load(link );
         });
     })
+
+jQuery(function($) {
+    //
+    $('#output').html('script here<'+'script>$(document).ready(function() {
+        // Fill modal with content from link href
+        $(".openModal").on("click", function(e) {
+            var link = $(this).data("href");
+        $('#popup').modal("show");
+        $('#popup .modal-body').load(link );
+        });
+    })</s' + 'cript>')
+
+});
