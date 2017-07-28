8e67c2134f19bb88f04260baee9e0a9188a52e27
José Osmar <ze_osmar@hotmail.com>
2017-07-28 16:50:01 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,9 @@
-$('[data-load-remote]').on('click',function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var remote = $this.data('load-remote');
-    if(remote) {
-        $($this.data('remote-target')).load(remote);
-    }
+$('a.modalButton').on('click', function(e) {
+    var src = $(this).attr('data-src');
+    var height = $(this).attr('data-height') || 300;
+    var width = $(this).attr('data-width') || 400;
+    
+    $("#myModal iframe").attr({'src':src,
+                        'height': height,
+                        'width': width});
 });
