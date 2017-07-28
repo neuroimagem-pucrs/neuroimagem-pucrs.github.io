9e1f0f459c75ba67b28fac23560d1a53daa79862
José Osmar <ze_osmar@hotmail.com>
2017-07-28 17:53:19 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,8 @@
-$('.openBtn').on('click',function(){
-    $('.modal-body').load('content.html',function(){
-        $('#myModal').modal({show:true});
-    });
+$(document).ready(function(){
+    $('.openPopup').on('click',function(){
+        var dataURL = $(this).attr('data-href');
+        $('.modal-body').load(dataURL,function(){
+            $('#myModal').modal({show:true});
+        });
+    }); 
 });
