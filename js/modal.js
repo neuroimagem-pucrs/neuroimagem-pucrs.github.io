e05b6807585da76aa315c93da60f0b5c884696c5
José Osmar <ze_osmar@hotmail.com>
2017-07-30 08:23:57 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-	
-// Support for AJAX loaded modal window.
-// Focuses on first input textbox after it loads the window.
-$('[data-toggle="modal"]').click(function(e) {
-	e.preventDefault();
-	var url = $(this).attr('href');
-	if (url.indexOf('#') == 0) {
-		$(url).modal('open');
-	} else {
-		$.get(url, function(data) {
-			$('<div class="modal hide fade">' + data + '</div>').modal();
-		}).success(function() { $('input:text:visible:first').focus(); });
-	}
-});
-	
+$('#myModal').on('show.bs.modal', function (e) {
+    var loadurl = $(e.relatedTarget).data('load-url');
+    $(this).find('.modal-body').load(loadurl);
 });
