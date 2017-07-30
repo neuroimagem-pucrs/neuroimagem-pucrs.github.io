dd6c268946728f33b3bcabc6b8fb516d51fa1b1e
José Osmar <ze_osmar@hotmail.com>
2017-07-30 11:18:54 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 /*$('#myModal').on('hidden.bs.modal', function () {
   $(this).removeData('bs.modal');
-});
+});*/
 
 $('#myModal').on('show.bs.modal', function () {
        $(this).find('.modal-body').css({
@@ -8,4 +8,4 @@ $('#myModal').on('show.bs.modal', function () {
               height:'auto', //probably not needed 
               'max-height':'100%'
        });
-});*/
+});
