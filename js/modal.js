60e7641eb43f97b9fb003c609660d5a443d87fe9
José Osmar <ze_osmar@hotmail.com>
2017-07-30 06:38:45 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,9 @@
-$('#mymodal').on('hidden', function () {
-  $(this).removeData('modal');
+$('a.modalButton').on('click', function(e) {
+    var src = $(this).attr('data-src');
+    var height = $(this).attr('data-height') || 300;
+    var width = $(this).attr('data-width') || 400;
+    
+    $("#myModal iframe").attr({'src':src,
+                        'height': height,
+                        'width': width});
 });
