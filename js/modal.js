97399c28515a69287a57ff1cc4d9e84f2d617312
José Osmar <ze_osmar@hotmail.com>
2017-08-13 14:11:33 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -9,11 +9,18 @@ $('#myModal').on('show.bs.modal', function () {
               'max-height':'100%'
        });
 });*/
-$('[data-load-remote]').on('click',function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var remote = $this.data('load-remote');
-    if(remote) {
-        $($this.data('remote-target')).load(remote);
-    }
-});
+    $('a[data-toggle="modal"]').on('click', function(){
+        // update modal header with contents of button that invoked the modal
+        $('#myModalLabel').html( $(this).html() );
+        //fixes a bootstrap bug that prevents a modal from being reused
+        $('#utility_body').load(
+            $(this).attr('href'),
+            function(response, status, xhr) {
+                if (status === 'error') {
+                    //console.log('got here');
+                    $('#utility_body').html('<h2>Oh boy</h2><p>Sorry, but there was an error:' + xhr.status + ' ' + xhr.statusText+ '</p>');
+                }
+                return this;
+            }
+        );
+    });
