415c5812699dee42fe541ae469b7e4b82357fb1f
José Osmar <ze_osmar@hotmail.com>
2017-08-13 18:35:33 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -23,7 +23,8 @@ $modal.on('show.bs.modal', function(e) {
       // Use Bootstrap's built-in function to fix scrolling (to no avail)
       $modal
         .removeClass('modal-scrollfix')
-        .modal('handleUpdate');
+        .modal('handleUpdate')
+    $('.modal-backdrop').remove();
         //.modal('hide');
       //$('body').removeClass('modal-open');
       
