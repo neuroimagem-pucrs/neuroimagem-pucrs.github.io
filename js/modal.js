a43d4be2d9b8ad805ef7b21acf819b76b83f8fd9
José Osmar <ze_osmar@hotmail.com>
2017-08-14 01:23:06 -0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -13,6 +13,7 @@ var $modal = $('.modal');
 
 // Show loader & then get content when modal is shown
 $modal.on('show.bs.modal', function(e) {
+   $("body").addClass("modal-open");
   var paragraphs = $(e.relatedTarget).data('paragraphs');
   
   $(this)
@@ -24,6 +25,9 @@ $modal.on('show.bs.modal', function(e) {
       //$modal
         //.removeClass('modal-scrollfix')
         //.modal('handleUpdate');  
-    $("body").addClass("modal-open");
+   
     });
+});
+$modal.on('hidden.bs.modal', function(e) {
+  $("body").removeClass("modal-open")
 });
